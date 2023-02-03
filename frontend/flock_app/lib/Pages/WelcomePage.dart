03a6b0cd31c4be3ff883ef62d19900fce4aebf56
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-02-03 18:35:30 -0500

    Welcome Page for users

--- frontend/flock_app/lib/Pages/WelcomePage.dart
+++ frontend/flock_app/lib/Pages/WelcomePage.dart
@@ -6,13 +6,66 @@ import '../main.dart';
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   // var appState = context.watch<MyAppState>();
-
-    return ListView(
+    return Scaffold (
+      body: ListView (
       shrinkWrap: true,
       children: [
-        Text("Welcome Page")
-      ],
+        Text(" "),
+        Text(" "),
+        Text(" "),
+        Text(" "),
+        Text(" "),
+        Image(image: AssetImage('assets/images/WelcomePageImage.png')),
+        Text(" "),
+        Text(" "),
+        Text(" "),
+        Row(children: [
+          Text("               "),
+          SizedBox (
+            height: 60,
+            width: 250,
+            child: ElevatedButton(
+                    style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    )
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 44, 155, 234)),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(10)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 14, color: Colors.white))),
+                    onPressed: () {},
+                      child: const Text('Sign Up', style: TextStyle(fontSize: 22))),
+            ),
+            Text(" "),
+          ]),
+          Text(" ", style: TextStyle(fontSize: 8)),
+          Row(children: [
+            Text("               "),
+            SizedBox (
+            height: 60,
+            width: 250,
+            child:
+          ElevatedButton(
+              style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              )
+              ),
+                    backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 4, 199, 59)),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(10)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 14, color: Colors.white))),
+                onPressed: () {},
+                child: const Text('Login', style: TextStyle(fontSize: 22))),
+                        ),
+                        Text(" "),
+          ]),
+    ])
     );
   }
 }
