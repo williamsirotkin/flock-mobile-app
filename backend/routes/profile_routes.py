3d0ab5e34e81ef17c591cd8926bbfc8a590c16bb
stephennemeth <stephennemeth4@gmail.com>
2023-02-03 18:52:24 -0500

    Didnt forget @ sign

--- backend/routes/profile_routes.py
+++ backend/routes/profile_routes.py
@@ -4,6 +4,6 @@ from db import db
 
 profile = Blueprint("profile", __name__, url_prefix="/profile")
 
-profile.route("/")
-def home():
+@profile.route("/")
+def profile_home():
     return "This is the profile routes"
