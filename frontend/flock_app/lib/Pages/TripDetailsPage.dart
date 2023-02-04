ea247633a73db0a3228285a841847f0fc246ecfc
RaymondFeckoury <82002364+RaymondFeckoury@users.noreply.github.com>
2023-02-04 12:50:41 -0500

    trip details page created

--- frontend/flock_app/lib/Pages/TripDetailsPage.dart
+++ frontend/flock_app/lib/Pages/TripDetailsPage.dart
@@ -5,12 +5,16 @@ import '../main.dart';
 import '../Widgets/BackButtonWidget.dart';
 import './FindTripsPage.dart';
 import '../Widgets/SuggestedPlaceWidget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class TripDetailsPage extends StatelessWidget {
   var user;
   TripDetailsPage({this.user});
+
   @override
   Widget build(BuildContext context) {
+    fetchTrip();
     return ListView(
       shrinkWrap: true,
       children: [
@@ -18,5 +22,21 @@ class TripDetailsPage extends StatelessWidget {
         Text("Trip Details Page"),
       ],
     );
+  }
+
+  Future<String> fetchTrip() async {
+    final response =
+        await http.get(Uri.parse('http://127.0.0.1:5000/trip/get/tripfive'));
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      print(jsonDecode(response.body));
+      Map<String, dynamic> jsonStuff = jsonDecode(response.body);
+      return jsonStuff['max_number'];
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load trip');
+    }
   }
 }
