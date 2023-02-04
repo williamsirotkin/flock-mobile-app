37c06616b98b1e87ce19a9f700d2efe6ab5cad71
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-02-03 21:48:28 -0500

    trip routes
    trip routes

--- backend/routes/trip_routes.py
+++ backend/routes/trip_routes.py
@@ -1,6 +1,5 @@
 from flask import Flask, Blueprint, request
 import json
-<<<<<<< Updated upstream
 from bson import json_util
 from db import db 
 
@@ -15,22 +14,3 @@ def trip_home():
 def get_trip(state):
     data = db.trip.find_one({'state': state})
     return json.loads(json_util.dumps(data))
-=======
-from db import db
-from bson import json_util
-
-trip = db.trip
-
-trip = Blueprint("trip", __name__, url_prefix="/trip")
-
-
-@trip.route("/")
-def trip_home():
-    return "This is the homepage for trips"
-
-@trip.route("/get/<str:California>")
-def getTrip(state):
-    trip = trip.find_one({"state" : 'California'})
-
-    return json.loads(json.util.dumps(trip))
->>>>>>> Stashed changes
