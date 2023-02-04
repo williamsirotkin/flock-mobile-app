1c598f1043501d2b458993d4bb0b7244614a4e1e
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-02-03 22:26:25 -0500

    sdfas
    asdfas

--- backend/routes/profile_routes.py
+++ backend/routes/profile_routes.py
@@ -1,13 +1,19 @@
 from flask import Flask, Blueprint, request
 import json
 from bson import json_util, ObjectId
-from db import db  
+from db import db
+import random  
 
 profile = Blueprint("profile", __name__, url_prefix="/profile")
 
 @profile.route("/")
 def profile_home():
     return "This is the profile routes"
+
+@profile.route("/getRandomUsername", methods = ['GET'])
+def get_random_username():
+    data = db.profile.find({}, {"username" : True})
+    return json.loads(json_util.dumps(random.choice(list(data))))
 
 @profile.route("/get/<string:username>", methods=['GET'])
 def get_profile(username):
