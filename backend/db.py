b922882c06dbacbfa09ed33876d29daf4612f6c0
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-02-03 21:33:40 -0500

    added trip data

--- backend/db.py
+++ backend/db.py
@@ -4,4 +4,3 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://marc_silverman:Flockapp69@cluster0.i8g2tz1.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 db = client.flock
-print(db.collection_names())
