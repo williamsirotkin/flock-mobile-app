70c188de0a3ccd779db4b8621d4d4b327e573da4
RaymondFeckoury <82002364+RaymondFeckoury@users.noreply.github.com>
2023-02-03 19:12:44 -0500

    Signup page template created

---
+++ frontend/flock_app/lib/Pages/SignUpPage.dart
@@ -0,0 +1,16 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../main.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // var appState = context.watch<MyAppState>();
+
+    return ListView(
+      shrinkWrap: true,
+      children: [Text("Signup Page")],
+    );
+  }
+}
--- frontend/flock_app/lib/main.dart
+++ frontend/flock_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './Pages/WelcomePage.dart';
 
 void main() {
