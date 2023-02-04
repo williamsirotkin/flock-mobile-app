f683bf5c391fdd308cf93f064972d2cde756e426
stephennemeth <stephennemeth4@gmail.com>
2023-02-03 19:41:07 -0500

    Fixed DB connection

--- backend/db.py
+++ backend/db.py
@@ -3,5 +3,5 @@ import os
 from pymongo import MongoClient
 
 MONGO_URI = os.environ['MONGO_URI']
-client = MongoClient(MONGO_URI, certifi.where())
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where()))
 db = client.db
